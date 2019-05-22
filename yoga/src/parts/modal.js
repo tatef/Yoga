76354a29be47f554d7df6b048162099c4089c6a9
tatef <35034739+tatef@users.noreply.github.com>
2019-05-22 12:56:26 +0300

    Add files via upload

--- yoga/src/parts/modal.js
+++ yoga/src/parts/modal.js
@@ -1,30 +1,27 @@
 function modal() {
-  let more = document.querySelector(".more"),
-    overlay = document.querySelector(".overlay"),
-    close = document.querySelector(".popup-close"),
-    descriptionBtn = document.querySelectorAll(".description-btn");
-  const showModal = () => {
-    more.addEventListener("click", () => {
-      overlay.style.display = "block";
-      more.classList.add("more-splash");
-      document.body.style.overflow = "hidden";
-    });
-    descriptionBtn.forEach(descriptionBtn => {
-      descriptionBtn.addEventListener("click", () => {
-        overlay.style.display = "block";
-        more.classList.add("more-splash");
-        document.body.style.overflow = "hidden";
-      });
-    });
-  };
-  const hideModal = () => {
-    close.addEventListener("click", () => {
-      overlay.style.display = "none";
-      more.classList.remove("more-splash");
-      document.body.style.overflow = "";
-    });
-  };
-  showModal();
-  hideModal();
+  let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+  more.classList.add('description-btn');
+  moreButtons = document.querySelectorAll('.description-btn');
+  close.addEventListener('click', hideModal);
+
+  for (let i = 0; i < moreButtons.length; i++) {
+    moreButtons[i].addEventListener('click', openModal);
+  }
+
+  function openModal() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function hideModal() {
+    overlay.style.display = 'none';
+    this.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  }
 }
+
 module.exports = modal;
